97b9311b5199eba4ada2098b80cedef3db69658f
Paweł Hajdo <pawel.hajdo001@gmail.com>
2023-12-19 22:20:18 +0100

    added play button, created script for menu buttons

---
+++ Assets/Scripts/MainMenuBtns.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+[RequireComponent(typeof(AudioSource))]
+
+public class MainMenuBtns : MonoBehaviour, IPointerUpHandler, IPointerDownHandler,
+IPointerEnterHandler, IPointerExitHandler
+{
+    public string levelToLoad;
+    public Sprite normalTexture;
+    public Sprite rollOverTexture;
+    public AudioClip beep;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        GetComponent<Image>().sprite = rollOverTexture;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        GetComponent<Image>().sprite = normalTexture;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        GetComponent<AudioSource>().PlayOneShot(beep);
+        SceneManager.LoadScene(levelToLoad);
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+    }
+}
